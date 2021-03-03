34c07e32c901f8b4268792db77e3f457898ad087
lispalding <lisette.spalding@gmail.com>
2021-03-03 14:20:42 -0700

    03/03/2021 -- 2:20PM

--- main.py
+++ main.py
@@ -10,15 +10,39 @@ from pygame.locals import *
 import random
 from colors import *
 from math import *
+import os
 ####### FIN #######
 
 ###### SETUP ######
+gameFolder = os.path.dirname(__file__)
+imageFolder = os.path.join(gameFolder, "images")
+playerImages = os.path.join(imageFolder, "player_image")
+
+soundFolder = os.path.join(gameFolder, "sounds")
+
+textDataFolder = os.path.join(gameFolder, "text_data")
+## VARIABLES
 WIDTH = 360
 HEIGHT = 480
 FPS = 30
 
+mouseBttnHeld = False
+
 title = "Template"
+## VARIABLES FIN
 ####### FIN #######
+
+###################### !! FUNCTIONS !!#####################
+def spawnPlayer(x, y):
+    newPlayer = Player()
+    newPlayer.rect.center = (x, y)
+    newPlayer.speedx = random.randint(-10, 10)
+    newPlayer.speedy = random.randint(-10, 10)
+    allSprites.add(newPlayer)
+    playersGroup.add(newPlayer)
+#################### !! FUNCTIONS FIN !!###################
+
+###################### !! CLASSES !!######################
 
 class NPC(pg.sprite.Sprite):
     def __init__(self):
@@ -131,8 +155,10 @@ class NPC(pg.sprite.Sprite):
 class Player(pg.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.image = pg.Surface((50, 50))
-        self.image.fill(PURPLE)
+        # self.image = pg.Surface((50, 50))
+        # self.image.fill(PURPLE)
+
+        self.image = playerImage
 
         self.rect = self.image.get_rect()
 
@@ -140,13 +166,81 @@ class Player(pg.sprite.Sprite):
         self.rect.center = (WIDTH/2, HEIGHT/2)
         ## Placement FIN
 
-        ### Normal speed variables
-        self.speedx = 5
-        self.speedy = 5
+        self.speedx = 0
+        self.speedy = 0
+
+        self.keypressed = False
+
+    def togglePressed(self):
+        self.keypressed = False
 
     def update(self):
+        """ To use: self.update()
+        This is the function that will update the movement of the player character. """
+        ##### !! Basic Movement !! #####
+        # self.speedx = 0
+        # self.speedy = 0
+
+        # Cheking the Keystate
+        keystate = pg.key.get_pressed()
+
+        ########## !!!! .. FLOW MOVEMENT .. !!!! ##########
+        # if keystate[pg.K_LEFT] or keystate[pg.K_a]:
+        #     self.speedx += -5
+        # if keystate[pg.K_RIGHT] or keystate[pg.K_d]:
+        #     player.speedx = 5
+        # if keystate[pg.K_UP] or keystate[pg.K_w]:
+        #      player.speedy = -5
+        # if keystate[pg.K_DOWN] or keystate[pg.K_s]:
+        #      player.speedy = 5
+        #
         self.rect.x += self.speedx
         self.rect.y += self.speedy
+        ########## !!!! .. FLOW FINISHED .. !!!! ##########
+
+        ####### !!!! .. BASIC GRID MOVEMENT .. !!!! #######
+        # if (keystate[pg.K_LEFT] or keystate[pg.K_a]) and self.keypressed == False:
+        #     self.keypressed = True
+        #     self.rect.centerx += -50
+        #
+        # if (keystate[pg.K_RIGHT] or keystate[pg.K_d]) and self.keypressed == False:
+        #     self.keypressed = True
+        #     self.rect.centerx += 50
+        #
+        # if (keystate[pg.K_UP] or keystate[pg.K_w]) and self.keypressed == False:
+        #     self.keypressed = True
+        #     self.rect.centery += -50
+        #
+        # if (keystate[pg.K_DOWN] or keystate[pg.K_s]) and self.keypressed == False:
+        #     self.keypressed = True
+        #     self.rect.centery += 50
+        ####### !!!! .. BASIC GRID FINISHED .. !!!! #######
+
+        ######### !!!! .. MOUSE MOVEMENT .. !!!! ##########
+        # self.rect.center = (mousex, mousey) # Move Everywhere
+        # self.rect.centerx = mousex  # Move Side to Side
+        # self.rect.centery = mousey # Move Up and Down
+
+        ########## .!. MOUSE CLICK AND DRAG
+        if mouseBttnHeld:
+            self.rect.center = (mousex, mousey)
+        ########## .!. MOUSE CLICK AND DRAG FIN
+
+        ######### !!!! .. MOUSE FINISHED .. !!!! ##########
+
+        ##### !!!! .. SCREEN BINDING .. !!!! #####
+        # We are binding the player to the screen area
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.right >= WIDTH:
+            self.rect.right = WIDTH
+        ##### !!!! .. BINDING FINISH .. !!!! #####
+
+##################### !! CLASSES FIN !!####################
 
 ####### GAME LOOP ########
 
@@ -158,6 +252,9 @@ pg.mixer.init()  # Sounds
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption(title)
 clock = pg.time.Clock()
+
+# Loading in Images
+playerImage = pg.image.load(os.path.join(playerImages, "player_img.jpg")).convert()
 
 # Creating sprite groups
 allSprites = pg.sprite.Group()
@@ -181,27 +278,59 @@ running = True
 while running:
     clock.tick(FPS) # Controlling the loop
 
+    mousex, mousey = pg.mouse.get_pos()
+
     ##### Processing input ######
     for event in pg.event.get(): # Getting a list of events that have happened
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT:
-                player.speedx = -5
-            elif event.key == pg.K_RIGHT:
-                player.speedx = 5
-            elif event.key == pg.K_UP:
-                player.speedy = -5
-            elif event.key == pg.K_DOWN:
-                player.speedy = 5
 
         if event.type == pg.KEYUP:
-            if event.key == pg.K_LEFT:
-                player.speedx = 0
-            elif event.key == pg.K_RIGHT:
-                player.speedx = 0
-            elif event.key == pg.K_UP:
-                player.speedy = 0
-            elif event.key == pg.K_DOWN:
-                player.speedy = 0
+            if (event.key == pg.K_LEFT) or (event.key == pg.K_a):
+                player.togglePressed()
+
+            if (event.key == pg.K_RIGHT) or (event.key == pg.K_d):
+                player.togglePressed()
+
+            if (event.key == pg.K_UP) or (event.key == pg.K_w):
+                player.togglePressed()
+
+            if (event.key == pg.K_DOWN) or (event.key == pg.K_s):
+                player.togglePressed()
+
+        if event.type == MOUSEBUTTONDOWN and \
+                player.rect.collidepoint(pg.mouse.get_pos()):
+            x = pg.mouse.get_pressed()
+
+            if x[0]:
+                mouseBttnHeld = True
+            if x[1]:
+                print("Mouse Wheel")
+            if x[2]:
+                spawnPlayer(mousex, mousey)
+
+        if event.type == MOUSEBUTTONUP and mouseBttnHeld == True:
+            mouseBttnHeld = False
+
+        ########## !!!! .. FLOW MOVEMENT .. !!!! ##########
+        # if event.type == pg.KEYDOWN:
+        #     if event.key == pg.K_LEFT:
+        #         player.speedx = -5
+        #     elif event.key == pg.K_RIGHT:
+        #         player.speedx = 5
+        #     elif event.key == pg.K_UP:
+        #         player.speedy = -5
+        #     elif event.key == pg.K_DOWN:
+        #         player.speedy = 5
+        #
+        # if event.type == pg.KEYUP:
+        #     if event.key == pg.K_LEFT:
+        #         player.speedx = 0
+        #     elif event.key == pg.K_RIGHT:
+        #         player.speedx = 0
+        #     elif event.key == pg.K_UP:
+        #         player.speedy = 0
+        #     elif event.key == pg.K_DOWN:
+        #         player.speedy = 0
+        ########## !!!! .. FLOW FINISHED .. !!!! ##########
 
 
         ####### !!!! .. BASIC GRID MOVEMENT .. !!!! #######
